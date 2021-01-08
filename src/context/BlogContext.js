3c3bfb8e3e50b89cfa6fe91c997b459184986eb8
amirulraziq <amirulraziqi@gmail.com>
2021-01-08 12:26:04 +0800

    Updated useState to useReducer within BlogContext to eliminate repeating setState code

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
+import React, { useReducer } from "react";
 
 // Creates a Context object which contains multiple child components, notably Provider and Consumer
 const BlogContext = React.createContext();
 
-// Provider component accepts a value prop to be passed down to 
+const blogReducer = (state, action) => {
+    // state === { data: object }
+    // action === { type: 'add' || 'edit' || 'delete' }
+
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog Post #${state.length + 1}`}];
+        default:
+            return state;
+    }
+}
+
+// Provider component accepts a value prop to be passed down to
 export const BlogProvider = ({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
-    const addBlogPost = () => {
-        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
-    };
+  const addBlogPost = () => {
+      dispatch({ type: 'add_blogpost' })
+  }    
 
-    return (
-        <BlogContext.Provider value={{ data: blogPosts, addBlogPost}}>
-            {children} 
-        </BlogContext.Provider>
-    );
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
