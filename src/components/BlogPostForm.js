ee6a78ef5efe15fb9296d8ccf670c5eddb2763db
amirulraziq <amirulraziqi@gmail.com>
2021-01-08 19:50:08 +0800

    send initialValues prop from EditScreen to BlogPostForm; to render blogPost values within EditScreen

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -2,20 +2,22 @@ import React, { useState, useContext } from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 
-const BlogPostForm = ({ onSubmit }) => {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState();
+const BlogPostForm = ({ onSubmit, initialValues }) => {
+    const [title, setTitle] = useState(initialValues.title);
+    const [content, setContent] = useState(initialValues.content);
 
   return (
     <View>
       <Text style={styles.label}>Enter Title: </Text>
       <TextInput
         style={styles.input}
+        value={title}
         onChangeText={(text) => setTitle(text)}
       />
       <Text style={styles.label}>Enter Content:</Text>
       <TextInput
         style={styles.input}
+        value={content}
         onChangeText={(text) => setContent(text)}
       />
       <Button
