f2a481efc0a742cd2c50f8f25498ff6a1d91390b
amirulraziq <amirulraziqi@gmail.com>
2021-01-08 12:42:30 +0800

    added useReducer for creating multiple Contexts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,33 +1,25 @@
-import React, { useReducer } from "react";
-
-// Creates a Context object which contains multiple child components, notably Provider and Consumer
-const BlogContext = React.createContext();
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
-    // state === { data: object }
-    // action === { type: 'add' || 'edit' || 'delete' }
+  // state === { data: object }
+  // action === { type: 'add_blogpost' || 'edit_blogpost' || 'delete_blogpost' }
 
-    switch (action.type) {
-        case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length + 1}`}];
-        default:
-            return state;
-    }
-}
-
-// Provider component accepts a value prop to be passed down to
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
-      dispatch({ type: 'add_blogpost' })
-  }    
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
 };
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
+  }
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { Context } from '../context/BlogContext';
 
 const IndexScreen = () => {
-    const { data, addBlogPost } = useContext(BlogContext); // access BlogContext object
+    const { state, addBlogPost } = useContext(Context); // access BlogContext object
     
     return (
         <View>
             <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList
-                data={data}
+                data={state}
                 keyExtractor={blogPost => blogPost.title} // where blogPost is each elem in blogPosts
                 renderItem={({ item }) => { // item refers to indivudal blogPost elem object
                     return <Text>{item.title}</Text>
