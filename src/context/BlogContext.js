b267bc234d362058cfdfc02051f1b359ca28387b
amirulraziq <amirulraziqi@gmail.com>
2021-01-08 11:52:26 +0800

    access BlogContext object in IndexScreen.js using useContext()

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+// Creates a Context object which contains multiple child components, notably Provider and Consumer
 const BlogContext = React.createContext();
 
+// Provider component accepts a value prop to be passed down to 
 export const BlogProvider = ({ children }) => {
     return (
-        <BlogContext.Provider>
+        <BlogContext.Provider value={[12,15]}>
             {children} 
         </BlogContext.Provider>
     );
 };
+
+export default BlogContext;
