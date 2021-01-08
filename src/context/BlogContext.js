4c79ec6f29848d732df17bacbea70b613d8f20d5
amirulraziq <amirulraziqi@gmail.com>
2021-01-08 12:15:48 +0800

    added blogPosts state in BlogProvider and hooked it up to IndexScreen to render blogposts using FlatList

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Creates a Context object which contains multiple child components, notably Provider and Consumer
 const BlogContext = React.createContext();
 
 // Provider component accepts a value prop to be passed down to 
 export const BlogProvider = ({ children }) => {
-    const blogPosts = [
-        { title: "Blog Post #1" },
-        { title: "Blog Post #2"}
-    ]
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+    };
 
     return (
-        <BlogContext.Provider value={blogPosts}>
+        <BlogContext.Provider value={{ data: blogPosts, addBlogPost}}>
             {children} 
         </BlogContext.Provider>
     );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-    const blogPosts = useContext(BlogContext); // access BlogContext object
+    const { data, addBlogPost } = useContext(BlogContext); // access BlogContext object
     
     return (
         <View>
             <Text>Index Screen</Text>
+            <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList
-                data={blogPosts}
+                data={data}
                 keyExtractor={blogPost => blogPost.title} // where blogPost is each elem in blogPosts
                 renderItem={({ item }) => { // item refers to indivudal blogPost elem object
                     return <Text>{item.title}</Text>
