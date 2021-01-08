5669238bd9233676fc9d72b3320f37e70bff6b2d
amirulraziq <amirulraziqi@gmail.com>
2021-01-08 18:11:21 +0800

    Added ShowScreen for individual blogpost

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,16 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { Context } from "../context/BlogContext";
 import { EvilIcons } from "@expo/vector-icons";
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context); // access BlogContext object
 
   return (
@@ -15,17 +22,21 @@ const IndexScreen = () => {
         renderItem={({ item }) => {
           // item refers to indivudal blogPost elem object
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title} - {item.id}</Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                <EvilIcons
+            <TouchableOpacity onPress={() => navigation.navigate("Show", { id: item.id })}>
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <EvilIcons
                     name="trash"
                     size={24}
                     color="black"
                     style={styles.icon}
-                />
-              </TouchableOpacity>
-            </View>
+                  />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           );
         }}
       />
